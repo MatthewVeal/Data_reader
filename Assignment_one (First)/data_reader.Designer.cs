70caa0f363378cbd910e59c33ced04c70b25009f
mattveal15 <m.veal2698@student.leedsbeckett.ac.uk>
2016-03-03 16:50:19 +0000

    Creation and text file load
    So far I've created my first form and got it to load the text file. Run
    a test to make sure that the file is reading the text inside the test
    file and not just loading a blank document.

---
+++ Assignment_one (First)/data_reader.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Assignment_one
+{
+    partial class data_reader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // data_reader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "data_reader";
+            this.Text = "Data Reader";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.data_reader_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Assignment_one (First)/data_reader.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_one
+{
+    public partial class data_reader : Form
+    {
+
+        //Load text File
+        Session_data session = new Session_data();
+        string filepath = @"F:\SE-B\Assignment_one\data.txt";   //Text file location path
+        string[] data;
+        List<Data_entry> data_list = new List<Data_entry>();
+
+        public data_reader()
+        {
+            InitializeComponent();
+        }
+
+        private void data_reader_Load(object sender, EventArgs e)
+        {
+            //Load data from text file
+            data = load_data();
+
+            if (data.Length > 0)
+            {
+            }
+            else
+            {
+                Console.WriteLine("Data Failed To Load");
+            }
+        }
+        
+        private string[] load_data()
+        {
+            string[] lines = System.IO.File.ReadAllLines(filepath);
+            return lines;
+        }
+              
+
+
+        public class Session_data
+        { }
+
+        public class Data_entry
+        { }
+
+
+
+    }
+}
+
