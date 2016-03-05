eabb0e15a970b0656bb2f228fbd0a29df334407c
mattveal15 <m.veal2698@student.leedsbeckett.ac.uk>
2016-03-05 14:06:56 +0000

    Added Graph
    I've now added the graph and it shows the line correctly, I now just
    need to add check boxes so i can filter through the data

---
+++ Assignment_one (fourth)/data_reader/data_reader.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace data_reader
+{
+    partial class data_reader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(data_reader));
+            this.dataView = new System.Windows.Forms.DataGridView();
+            this.version = new System.Windows.Forms.TextBox();
+            this.start_time = new System.Windows.Forms.TextBox();
+            this.date = new System.Windows.Forms.TextBox();
+            this.length = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataView
+            // 
+            this.dataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataView.Location = new System.Drawing.Point(12, 55);
+            this.dataView.Name = "dataView";
+            this.dataView.Size = new System.Drawing.Size(771, 539);
+            this.dataView.TabIndex = 4;
+            // 
+            // version
+            // 
+            this.version.Location = new System.Drawing.Point(12, 29);
+            this.version.Name = "version";
+            this.version.Size = new System.Drawing.Size(103, 20);
+            this.version.TabIndex = 15;
+            // 
+            // start_time
+            // 
+            this.start_time.Location = new System.Drawing.Point(213, 600);
+            this.start_time.Name = "start_time";
+            this.start_time.Size = new System.Drawing.Size(194, 20);
+            this.start_time.TabIndex = 14;
+            // 
+            // date
+            // 
+            this.date.Location = new System.Drawing.Point(680, 29);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(103, 20);
+            this.date.TabIndex = 13;
+            // 
+            // length
+            // 
+            this.length.Location = new System.Drawing.Point(14, 600);
+            this.length.Name = "length";
+            this.length.Size = new System.Drawing.Size(193, 20);
+            this.length.TabIndex = 12;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(796, 25);
+            this.toolStrip1.TabIndex = 16;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(45, 22);
+            this.toolStripDropDownButton1.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(664, 600);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 31);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Graph";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // data_reader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(796, 643);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.version);
+            this.Controls.Add(this.start_time);
+            this.Controls.Add(this.date);
+            this.Controls.Add(this.length);
+            this.Controls.Add(this.dataView);
+            this.Name = "data_reader";
+            this.Text = "Data Reader";
+            this.Load += new System.EventHandler(this.data_reader_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataView;
+        private System.Windows.Forms.TextBox version;
+        private System.Windows.Forms.TextBox start_time;
+        private System.Windows.Forms.TextBox date;
+        private System.Windows.Forms.TextBox length;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
